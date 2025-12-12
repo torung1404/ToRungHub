d4a217a2758120e8e1dd18200a4cbf18aa0b250f
torung1404 <fakeshoot111@gmail.com>
2025-12-13 00:58:25 +0700

    ToRungHub: Logger.lua (Shared)

---
+++ Logger.lua
@@ -0,0 +1,37 @@
+--!strict
+local Logger = {}
+Logger.__index = Logger
+
+local LEVELS = {
+	ERROR = 1,
+	WARN = 2,
+	INFO = 3,
+	DEBUG = 4,
+}
+
+function Logger.new(scope: string, levelName: string)
+	local self = setmetatable({}, Logger)
+	self.scope = scope
+	self.level = LEVELS[levelName] or LEVELS.INFO
+	return self
+end
+
+function Logger:withScope(scope: string)
+	return Logger.new(scope, (self.level == 4 and "DEBUG") or (self.level == 3 and "INFO") or (self.level == 2 and "WARN") or "ERROR")
+end
+
+local function now()
+	return os.date("!%H:%M:%S")
+end
+
+function Logger:_print(minLevel: number, tag: string, msg: string)
+	if self.level < minLevel then return end
+	print(("[ToRungHub][%s][%s][%s] %s"):format(now(), self.scope, tag, msg))
+end
+
+function Logger:error(msg: string) self:_print(LEVELS.ERROR, "ERR", msg) end
+function Logger:warn(msg: string) self:_print(LEVELS.WARN, "WRN", msg) end
+function Logger:info(msg: string) self:_print(LEVELS.INFO, "INF", msg) end
+function Logger:debug(msg: string) self:_print(LEVELS.DEBUG, "DBG", msg) end
+
+return Logger
